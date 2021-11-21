fc29229f844d868326e3776af8797021766abd20
Văn Khắc Huy <huyvk@vmodev.com>
2021-11-21 15:55:31 +0700

    Swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // > Versioning
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  // > Swagger
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Nest example')
+    .setDescription('The nestjs example project')
+    .setVersion('1.0')
+    .build();
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentSwagger);
 
   await app.listen(3000);
 }
