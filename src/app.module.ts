7119f3e394ac66ba5a8782be1250ccbac792762a
Văn Khắc Huy <huyvk@vmodev.com>
2021-11-21 14:56:54 +0700

    Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
